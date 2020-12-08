6065f6f910eb456b65116702b95443e351d673b4
Christopher <chrisjlee250@gmail.com>
2020-12-08 15:33:49 -0800

    product sum problem

---
+++ productSum.js
@@ -0,0 +1,15 @@
+// ALGO EXPERT PRODUCT SUM PROBLEM
+// o(n) time, o(d) space, where D is the depth
+
+function productSum(array, totalDepth = 1) {
+	let totalSum = 0; 
+	for (let i = 0; i < array.length; i++) { 
+		let el = array[i]; 
+		if (el instanceof Array) {
+			totalSum += productSum(el, totalDepth + 1) 
+		}	else {
+			totalSum += el; 
+		}
+	}
+	return totalSum * totalDepth;
+}
