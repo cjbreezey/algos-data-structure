2e26973dc3b808e6b9d50906ea8e71104f8f46f4
Christopher <chrisjlee250@gmail.com>
2021-02-10 10:39:27 -0800

    optimized product of array except self

--- Leet Code/productArrayExceptSelf.js
+++ Leet Code/productArrayExceptSelf.js
@@ -24,3 +24,20 @@ var productExceptSelf = function (nums) {
 
 // o(n) time complexity
 
+var productExceptSelf = function (nums) {
+    const length = nums.length;
+    const answer = Array(length).fill(1);
+    const leftp = Array(length).fill(1); // to store all product to the left of i
+    const rightp = Array(length).fill(1); // to store all profuct to the right of i
+    // so we loop through i, just product the left and right to the i
+    for (let i = 1; i < leftp.length; i++) {
+        leftp[i] = nums[i - 1] * leftp[i - 1];
+    }
+    for (let j = rightp.length - 2; j >= 0; j--) {
+        rightp[j] = nums[j + 1] * rightp[j + 1];
+    }
+    for (let i = 0; i < nums.length; i++) {
+        answer[i] = leftp[i] * rightp[i];
+    }
+    return answer;
+};
