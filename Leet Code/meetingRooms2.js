7ca21d44dd0d184eec6063dc88be7c5201a4ab79
Christopher <chrisjlee250@gmail.com>
2021-03-10 22:16:10 -0800

    meeting rooms 2 leetcode

---
+++ Leet Code/meetingRooms2.js
@@ -0,0 +1,39 @@
+// Given an array of meeting time intervals intervals where intervals[i] = [starti, endi], return the minimum number of conference rooms required.
+
+
+
+//     Example 1:
+
+// Input: intervals = [[0, 30], [5, 10], [15, 20]]
+// Output: 2
+// Example 2:
+
+// Input: intervals = [[7, 10], [2, 4]]
+// Output: 1
+
+var minMeetingRooms = function (intervals) {
+    //     let result = 0;
+    //     let start_time = [];
+    //     let end_time = [];
+    //     for (let i = 0; i < intervals.length; i++) {
+    //         start_time.push(intervals[i][0])
+    //         end_time.push(intervals[i][1])
+    //     }
+    //     start_time.sort((a,b) => a - b)
+    //     end_time.sort((a,b) => a - b)
+
+    //     let i = 0;
+    //     let j = 0;
+    //     let meetingRooms = 0;
+    //     while (i < start_time.length) {
+    //         if (start_time[i] < end_time[j]) {
+    //             meetingRooms++
+    //             i++
+    //         } else {
+    //             meetingRooms--
+    //             j++
+    //         }
+    //         result = Math.max(result, meetingRooms)
+    //     }
+    //     return result
+};
