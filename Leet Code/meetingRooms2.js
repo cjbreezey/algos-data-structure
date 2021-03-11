758f4379fc3c47c5de6a076d2b46a2637adb2f81
Christopher <chrisjlee250@gmail.com>
2021-03-10 22:16:31 -0800

    alternative with sort meeting rooms 2 leetcode

--- Leet Code/meetingRooms2.js
+++ Leet Code/meetingRooms2.js
@@ -36,4 +36,21 @@ var minMeetingRooms = function (intervals) {
     //         result = Math.max(result, meetingRooms)
     //     }
     //     return result
+
+    var starts = intervals.concat().sort(function (a, b) {
+        return a[0] - b[0];
+    });
+    var ends = intervals.sort(function (a, b) {
+        return a[1] - b[1];
+    });
+    var rooms = 0;
+    var end = 0;
+    for (var i = 0; i < intervals.length; i++) {
+        if (starts[i][0] < ends[end][1]) {
+            rooms++;
+        } else {
+            end++;
+        }
+    }
+    return rooms;
 };
