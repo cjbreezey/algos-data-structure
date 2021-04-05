7435d87600bf7690dbaad81d28e377c8da6ccc17
Christopher <chrisjlee250@gmail.com>
2021-04-05 15:53:49 -0700

    valid palindrome with regexp and 2 pointer method

--- Leet Code/validPalindrome.js
+++ Leet Code/validPalindrome.js
@@ -1,3 +1,5 @@
+// naive .reverse() method
+
 var isPalindrome = function (s) {
     let alpha = 'abcdefghijklmnopqrstuvwxyz0123456789'
     let string = ""
@@ -9,4 +11,23 @@ var isPalindrome = function (s) {
     }
     console.log(string)
     return string === string.split('').reverse().join('')
+};
+
+// two pointer and regexp approach
+
+var isPalindrome = function (s) {
+    const regex = /[^0-9a-z]/gi;
+    const cleaned = [...s.toLowerCase().replace(regex, "")];
+    let left = 0;
+    let right = cleaned.length - 1;
+    console.log(cleaned)
+    while (left < right) {
+        if (cleaned[left] !== cleaned[right]) {
+            return false
+        }
+        left++
+        right--
+    }
+    return true
+
 };
