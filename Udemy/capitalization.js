2b94d297a1277a0c755ca325b26edccfdb343c2e
Christopher <chrisjlee250@gmail.com>
2021-04-07 14:28:40 -0700

    added second method of capitalization

--- Udemy/capitalization.js
+++ Udemy/capitalization.js
@@ -3,6 +3,21 @@
 // o(n) space where n is the length of the string
 
 function capitalize(str) {
+    let result = str[0].toUppercase();
+
+    for (let i = 1; i < str.length; i++) {
+        if (str[i - 1] === ' ') {
+            result += str[i].toUppercase();
+        } else {
+            result += str[i]
+        }
+    }
+
+    return result
+
+}
+
+function capitalize(str) {
     let result = [];
     for (let word of str.split(' ')) {
         result.push(word[0].toUpperCase() + word.slice(1))
