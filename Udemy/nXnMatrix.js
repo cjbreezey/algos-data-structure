660461bfbdff2b22d1b02a83525de26ae99beec2
Christopher <chrisjlee250@gmail.com>
2021-04-07 16:16:02 -0700

    spiral matrix commenting for easier understanding later

--- Udemy/nXnMatrix.js
+++ Udemy/nXnMatrix.js
@@ -3,27 +3,34 @@ function matrix(n) {
     for (let i = 0; i < n; i++) {
         result.push([]);
     }
-    let counter = 1;
-    let startRow = 0;
-    let startCol = 0;
-    let endRow = n - 1;
-    let endCol = n - 1;
+
+    // result = [ [1, 2, 3, 4], [12, null, null, 5], [11, null, null, 6], [10, 9, 8, 7] ]
+
+    let counter = 1; // 13
+    let startRow = 0; // 1
+    let startCol = 0; // 1
+    let endRow = n - 1; // 2
+    let endCol = n - 1; // 2
     while (startCol <= endCol && startRow <= endRow) {
-        for (let i = startCol; i <= endCol; i++) {
+        // Top Left Row
+        for (let i = startCol; i <= endCol; i++) { 
             result[startCol][i] = counter;
             counter++;
         }
         startRow++;
+        // Top Right Col
         for (let j = startRow; j <= endRow; j++) {
             result[j][endCol] = counter;
             counter++;
         }
         endCol--;
+        // Bottom Left Row
         for (let i = endCol; i >= startCol; i--) {
             result[endRow][i] = counter;
             counter++;
         }
         endRow--;
+        // Bottom Right Col
         for (let i = endRow; i >= startRow; i--) {
             result[i][startCol] = counter
             counter++
