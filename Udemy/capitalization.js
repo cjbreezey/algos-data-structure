39368ab644083770b55f0b2c1c96fef4fccff0b1
Christopher <chrisjlee250@gmail.com>
2021-04-07 14:25:48 -0700

    udemy capitalization

--- Udemy/capitalization.js
+++ Udemy/capitalization.js
@@ -3,16 +3,9 @@
 // o(n) space where n is the length of the string
 
 function capitalize(str) {
-    let result = str[0].toUppercase();
-
-    for (let i = 1; i < str.length; i++) {
-        if (str[i - 1] === ' ') {
-            result += str[i].toUppercase();
-        } else {
-            result += str[i]
-        }
+    let result = [];
+    for (let word of str.split(' ')) {
+        result.push(word[0].toUpperCase() + word.slice(1))
     }
-
-    return result
-
+    return result.join(' ')
 }
